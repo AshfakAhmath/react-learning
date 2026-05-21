6ea32829ef558318375acb4f0a2a8148204ab611
AshfakAhmath <ashfakahmath57@gmail.com>
2026-05-21 17:52:50 +0530

    Add Student component and styles, integrate into App component

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -4,6 +4,7 @@ import Card from "./Card";
 import Food from "./Food";
 import Footer from "./Footer";
 import Header from "./Header";
+import Student from "./Student";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
       <Button />
       <Button1 />
       <Footer />
+      <Student name="Ashfak Ahmath" age={22} isStudent={true} />
+      <Student name="Sahi" age={23} isStudent={false} />
+      <Student name="Aasik" age={21} isStudent={true} />
     </>
   );
 }
